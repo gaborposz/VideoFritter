8f0de41075e4390eeaa0a45ad98001d7e929248c
Gabor Posz <gabor.posz@gmail.com>
2019-03-15 20:05:36 +0100

    New Audio Volume control

---
+++ VideoFritter/Controls/AudioVolumeControl/AudioVolumeControl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace VideoFritter.Controls.AudioVolumeControl
+{
+    public class AudioVolumeControl : ProgressBar
+    {
+        static AudioVolumeControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AudioVolumeControl), new FrameworkPropertyMetadata(typeof(AudioVolumeControl)));
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            this.MouseLeftButtonDown += AudioVolumeControl_MouseLeftButtonDown;
+            this.MouseMove += AudioVolumeControl_MouseMove;
+        }
+
+        private void AudioVolumeControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                UpdateProgressBarValue((ProgressBar)sender, e);
+            }
+        }
+
+        private void AudioVolumeControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            UpdateProgressBarValue((ProgressBar)sender, e);
+        }
+
+        private void UpdateProgressBarValue(ProgressBar progressBar, MouseEventArgs e)
+        {
+            Point mousePosition = e.GetPosition(progressBar);
+            progressBar.Value = progressBar.Maximum * (progressBar.ActualHeight - mousePosition.Y) / progressBar.ActualHeight;
+        }
+    }
+}
