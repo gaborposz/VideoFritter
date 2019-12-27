d0de483f6399f5dd8b4dad4b210befe664614faa
Gabor Posz <gabor.posz@gmail.com>
2019-12-27 21:10:20 +0100

    Settings window header was removed as well + small Export Queue window fixes.

--- VideoFritter/Settings/SettingsDialog.xaml.cs
+++ VideoFritter/Settings/SettingsDialog.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace VideoFritter.Settings
 {
@@ -27,6 +16,14 @@ namespace VideoFritter.Settings
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void WindowDragging(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
         }
     }
 }
